2d136c3e190f7f671c3a621688b5c8a606c7224b
RPiDASTeam <rpidaseditor@gmail.com>
2009-01-06 09:20:40 +0000

    v3.15

--- datalogger/datetime/setTime.sh
+++ datalogger/datetime/setTime.sh
@@ -3,4 +3,4 @@
 # Seconds from Jan 1 1900 (Network Time Protocol epoch) to Jan 1 1970 (Unix epoch)
 timeShift=2208988800
 
-sudo date -s @$(($((0x$(xxd -ps /KWH/datalogger/datetime/nisttime))) - $timeShift))
+sudo date -s @$(($((0x$(xxd -ps /KWH/datalogger/datetime/nisttime))) - $timeShift)) >> /KWH/datalogger/datetime/datetime.log
