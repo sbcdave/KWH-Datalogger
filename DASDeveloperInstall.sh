c434cf0f558568f28192d2d0122f16fc7fdd0cdb
sbcdave <ggoldsmith@gmail.com>
2018-05-06 05:05:48 +0000

    v73

--- DASDeveloperInstall.sh
+++ DASDeveloperInstall.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
-echo "This script installs hub for to simplify development via GitHub"
-echo "Credit https://github.com/github/hub.git"
+echo ""
+echo "This script installs hub to simplify DAS development via GitHub"
+echo "Credit https://github.com/github/hub.git and Google"
 echo "Downloading GoLang for hub install..."
+echo ""
 wget https://dl.google.com/go/go1.10.2.linux-armv6l.tar.gz
 wait
 echo "Unpacking Go..."
-tar -xzf go1.10.2.linux-armv61.tar.gz
+tar -xzf /KWH/go1.10.2.linux-armv6l.tar.gz
 wait
+echo ""
 echo "Moving Go binary to a location that is in \$PATH"
 sudo cp /KWH/go/bin/go /usr/sbin/.
 wait
+echo ""
 echo "Downloading hub..."
 git clone https://github.com/github/hub.git
 wait
+echo ""
 echo "Installing hub..."
 /KWH/hub/script/build
 wait
+echo ""
 echo "Aliasing hub as git..."
 echo eval \"\$\(hub alias -s\)\" >> ~/.bashrc
 . ~/.bashrc
--- installer.sh
+++ installer.sh
@@ -1,45 +1,98 @@
 #!/bin/bash
 
 # Install git for pulling the code from the repository
-sudo apt-get install git
+echo "Installing git"
+wait
+sudo apt-get install -y git
+wait
 
 # Move to root and download data logger code from github
 cd /
+echo ""
+echo "Downloading data logger software to the root directory in /KWH"
+wait
 sudo git clone https://github.com/sbcdave/KWH.git
+wait
 
 # Change data logger code root directory owner:group to pi
+echo ""
+echo "Updating /KWH permissions"
+wait
 sudo chown -R pi:pi KWH
-
+wait
 
 #investigate shutting down uneccesary services
 
 # Create symlink from /etc/defaults to datalogger.conf file
+echo ""
+echo "Ensuring datalogger config is used at boot"
+wait
 sudo ln -n /KWH/datalogger/config/datalogger.conf /etc/default/datalogger.conf
-
+wait
 # Add datalogger.conf file to /etc/profile and to root and pi's .bashrc
 sudo printf "\n. /KWH/datalogger/config/datalogger.conf\n" >> /etc/profile
+wait
 sudo printf "\n. /KWH/datalogger/config/datalogger.conf\n" >> /root/.bashrc
+wait
 printf "\n. /KWH/datalogger/config/datalogger.conf\n" >> /home/pi/.bashrc
-
+wait
 # Source the datalogger.conf file into environment variables
 . /KWH/datalogger/config/datalogger.conf
+wait
 
 # Activate 1 minute transmission via cron
+echo ""
+echo "Enabling cron jobs"
+wait
 sudo cp /KWH/datalogger/moves/dcrond /etc/cron.d/.
+wait
 sudo chmod 644 /etc/cron.d/dcrond
+wait
 
 # Setting up SIM communications on ttyAMA0
+echo ""
+echo "Enabling ttyAMA0 on TX/RX for SIM comms"
+wait
 sudo systemctl mask serial-getty@ttyAMA0.service
+wait
 
 # Adjusting /boot/config.txt for SIM and Temp sensors communications
+echo ""
+echo "Updating /boot/config.txt to enable SIM and Temp sensor comms"
+wait
 sudo cp /boot/config.txt /KWH/.
+wait
 sudo chown pi:pi /KWH/config.txt
+wait
 echo "
 #Data Logger
 dtoverlay=pi3-disable-bt
 enable_uart=1
 force_turbo=1
 dtoverlay=w1-gpio" >> /KWH/config.txt
+wait
 sudo cp /KWH/config.txt /boot/config.txt
-rm /KWH/config.txt
+wait
+
+# Enable simserver.service
+echo ""
+echo "Enabling sim server service"
+sudo cp /KWH/moves/simserver.service /etc/systemd/system/.
+wait
+sudo systemctl enable simserver.service
+wait
+sudo systemctl start simserver.service
+wait
+
+# Reboot to finalize
+echo ""
+echo "Installation complete. Reboot to enable communications with PCB.
+echo "Reboot now?(y)"
+read ans
+if [ "$ans" = "y" ]; then
+    echo "Thank you, and enjoy...courtesy of KiloWatts for Humanity"
+    sudo shutdown -r now
+else
+    echo "Error logs will fill with communications issues until reboot"
+fi
 
